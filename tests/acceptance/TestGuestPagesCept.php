02f6ffd0fb7701d83af60acdc976bc92929297d1
Andrey Majoroff <andrey.majoroff@gmail.com>
2018-11-30 23:14:51 +0200

    add bootstrap navbar

--- tests/acceptance/TestGuestPagesCept.php
+++ tests/acceptance/TestGuestPagesCept.php
@@ -4,11 +4,11 @@ $I->wantTo('Open home/join/login pages');
 $I->amOnPage('/');
 $I->see('Index', 'h1'); // Why Welcome don't work?
 
-//$I->seeLink('Join', '/site/join');
-//$I->seeLink('Login', '/site/login');
-//
-//$I->amOnPage('/site/join');
-//$I->see('Join us', 'h1');
-//
-//$I->amOnPage('/site/login');
-//$I->see('Log in', 'h1');
+$I->seeLink('Join', '/site/join');
+$I->seeLink('Login', '/site/login');
+
+$I->amOnPage('/site/join');
+$I->see('Join us', 'h1');
+
+$I->amOnPage('/site/login');
+$I->see('Log in', 'h1');
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,13 +1,58 @@
 <!doctype html>
+
+<?php
+$this->beginPage();
+?>
+
 <html lang="en">
+
 <head>
+    <?php
+    $this->head();
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>VideoSchool</title>
+
 </head>
+
+<?php
+
+use yii\bootstrap\NavBar;
+
+?>
+
 <body>
+<?php
+$this->beginBody();
+?>
+    <?php
+
+    NavBar::begin([
+            'brandLabel' => 'VideoSchool',
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                    'class' => 'navbar-default navbar-fixed-top'
+            ]
+
+    ]);
+    NavBar::end();
+    ?>
+
+<div class="container" style="margin-top: 5%">
+
     <?= $content ?>
+
+</div>
+
+<?php
+$this->endBody();
+?>
 </body>
 </html>
+<?php
+$this->endPage();
+?>
