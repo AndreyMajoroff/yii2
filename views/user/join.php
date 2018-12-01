3a3e8ed2c18ef549e0b07d0b77a147dfc05fbe5d
Andrey Majoroff <andrey.majoroff@gmail.com>
2018-12-01 12:54:50 +0200

    add div panel to login & join pages

--- views/user/join.php
+++ views/user/join.php
@@ -1,1 +1,12 @@
-<h1>Join</h1>
+<div class="panel panel-warning">
+    <div class="panel-heading">
+        <h1>Join</h1>
+    </div>
+<div class="panel-body">
+    name<br>
+    email<br>
+    password<br>
+    confirm password<br>
+    [Create]
+</div>
+</div>
