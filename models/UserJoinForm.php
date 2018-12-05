42d60b94fed09ee824d7b5697d55755bec82c49d
Andrey Majoroff <andrey.majoroff@gmail.com>
2018-12-05 09:39:23 +0200

    add UserJoinForm model & html form

---
+++ models/UserJoinForm.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace app\models;
+
+
+use yii\base\Model;
+
+class UserJoinForm extends Model
+{
+    public $name;
+    public $email;
+    public $password;
+    public $confirmPassword;
+}
--- views/user/join.php
+++ views/user/join.php
@@ -1,12 +1,23 @@
+<?php
+use yii\widgets\ActiveForm;
+use yii\helpers\Html;
+?>
+
 <div class="panel panel-warning">
     <div class="panel-heading">
         <h1>Join</h1>
     </div>
 <div class="panel-body">
-    name<br>
-    email<br>
-    password<br>
-    confirm password<br>
-    [Create]
+
+  <?php $form = ActiveForm::begin(['id' => 'user-join-form'])?>
+  <?= $form->field($userJoinForm, 'name') ?>
+  <?= $form->field($userJoinForm, 'email') ?>
+  <?= $form->field($userJoinForm, 'password')->passwordInput() ?>
+  <?= $form->field($userJoinForm, 'confirmPassword')->passwordInput() ?>
+  <?= Html::submitButton('Create',
+      ['class' => 'btn btn-danger']) ?>
+
+    <?php $form = \yii\widgets\ActiveForm::end();?>
+
 </div>
 </div>
